4d52009e3d3431e3bac133722ec0bddd2c6006e6
Clement Trosa <me@trosa.io>
2018-05-08 13:08:12 +0200

    Updated dependencies.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [clj-http "3.7.0"]
+                 [clj-http "3.9.0"]
                  [cheshire "5.8.0"]
                  [com.cemerick/url "0.1.1"]
                  [org.clojure/data.codec "0.1.1"]
